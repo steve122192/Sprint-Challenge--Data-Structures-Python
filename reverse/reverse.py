39dec25b8e81eed9fc10a5329b3ce3138e9d9fe3
Steven Reiss <sgreiss92@gmail.com>
2020-06-12 13:29:47 -0400

    sprint challenge mvp

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,18 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if self.head is None:
+            return
+        if node.next_node is None:
+            self.head = node
+            node.next_node = prev
+            return
+        
+        next = node.next_node
+
+        node.next_node = prev
+        
+        self.reverse_list(next, node)
+
+        
+
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.count = 0
 
     def append(self, item):
-        pass
-
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        else:
+            self.storage.pop(self.count)
+            self.storage.insert(self.count,item)
+            self.count += 1
+            if self.count == self.capacity:
+                self.count = 0
+            
+    
     def get(self):
-        pass
+        return self.storage
